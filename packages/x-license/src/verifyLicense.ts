9545b6c9a7e61fe514734ca60ed64a4741853c85
Andrew Maule <developer@bliptrip.net>
2021-07-06 19:54:45 -0500

    Changes for local use.

--- packages/x-license/src/verifyLicense.ts
+++ packages/x-license/src/verifyLicense.ts
@@ -12,46 +12,5 @@ export function generateReleaseInfo() {
 const expiryReg = /^.*EXPIRY=([0-9]+),.*$/;
 
 export function verifyLicense(releaseInfo: string, encodedLicense: string) {
-  if (!releaseInfo) {
-    throw new Error(
-      'Material-UI: The release information is missing. Not able to validate license.',
-    );
-  }
-
-  if (!encodedLicense) {
-    return LicenseStatus.NotFound;
-  }
-
-  const hash = encodedLicense.substr(0, 32);
-  const encoded = encodedLicense.substr(32);
-
-  if (hash !== md5(encoded)) {
-    return LicenseStatus.Invalid;
-  }
-
-  const clearLicense = base64Decode(encoded);
-  let expiryTimestamp = 0;
-  try {
-    expiryTimestamp = parseInt(clearLicense.match(expiryReg)![1], 10);
-    if (!expiryTimestamp || Number.isNaN(expiryTimestamp)) {
-      console.error('Error checking license. Expiry timestamp not found or invalid!');
-      return LicenseStatus.Invalid;
-    }
-  } catch (err) {
-    console.error('Error extracting license expiry timestamp.', err);
-    return LicenseStatus.Invalid;
-  }
-
-  const pkgTimestamp = parseInt(base64Decode(releaseInfo), 10);
-  if (Number.isNaN(pkgTimestamp)) {
-    throw new Error(
-      'Material-UI: The release information is invalid. Not able to validate license.',
-    );
-  }
-
-  if (expiryTimestamp < pkgTimestamp) {
-    return LicenseStatus.Expired;
-  }
-
-  return LicenseStatus.Valid;
+    return LicenseStatus.Valid;
 }
